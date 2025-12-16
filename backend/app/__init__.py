2071a8e6e4a510f7b4798003aab78f18ba25d767
BKNR.SOLUTIONS <priyaranjan@BKNRSOLUTIONS.local>
2025-12-16 13:00:53 +0530

    fix: remove sqlalchemy from app init

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,20 +1,2 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+# app package init
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./bknr_erp.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
