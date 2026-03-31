5f92610b0ae77991f2d40029e736df8673167c82
BKNR.SOLUTIONS <priyaranjan@BKNRSOLUTIONS.local>
2026-03-31 05:30:44 +0530

    debug database url

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
-
+print("DATABASE_URL:", DATABASE_URL)
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL not set")
 
