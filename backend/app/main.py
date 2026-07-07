65f488553fe6e4d9d8b173f01caaeb59afff55ea
Garikina Nagaraju <bknr.solutions@gmail.com>
2026-07-07 16:33:47 +0530

    fix: import Body from fastapi in main.py to fix deployment NameError

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, Depends
+from fastapi import FastAPI, Request, Depends, Body
 from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse, PlainTextResponse, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
