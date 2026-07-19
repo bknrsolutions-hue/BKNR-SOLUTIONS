2281fb299a04854a53a9460a2da721e154b83f21
Garikina Nagaraju <bknr.solutions@gmail.com>
2026-07-19 11:23:52 +0530

    Set Export Documents and Gate Entry form grids to 6 columns per row (repeat(6, minmax(108px, 1fr))) and 6px gap

---
+++ frontend/dist/assets/ExportApprovals-CoKt0iC8.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-DZ4YD7UV.js";import t from"./SupportingDocuments-vKz7PjMl.js";var n=e();function r(){return(0,n.jsx)(t,{initialStatus:`PENDING_APPROVAL`,pageTitle:`Export Document Approvals`,pageSubtitle:`Review documents assigned to you and complete approval decisions.`,approvalsOnly:!0})}export{r as default};
--- frontend/dist/assets/chevron-right-CfRsJeYN.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./createLucideIcon-Ia_nNzEt.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
